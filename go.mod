c6b94ede5f69997b406bbcd1737ccfb65499787c
DeVerl Stokes <deverl_stokes@intuit.com>
2024-03-13 13:30:42 -0600

    First version of toolkit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deverl/toolkit
+
+go 1.22.1
---
+++ tools.go
@@ -0,0 +1,21 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is the type used to instantiate this module.
+// Any variable of this type will have access to all methods with receiver *Tools.
+type Tools struct{}
+
+// Returns a string of length n of random characters, using randomStringSource
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
